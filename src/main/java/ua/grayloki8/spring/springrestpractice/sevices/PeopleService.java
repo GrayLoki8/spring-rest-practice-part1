77dfd2c384975b7075a1df29796fc48191b9660b
Gray Loki <skuc090@gmail.com>
2022-07-08 13:49:30 +0300

    add PeopleService

---
+++ src/main/java/ua/grayloki8/spring/springrestpractice/sevices/PeopleService.java
@@ -0,0 +1,26 @@
+package ua.grayloki8.spring.springrestpractice.sevices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ua.grayloki8.spring.springrestpractice.models.Person;
+import ua.grayloki8.spring.springrestpractice.repositories.PeopleRepository;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class PeopleService {
+
+   private PeopleRepository peopleRepository;
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+    public List<Person> findAll(){
+        return peopleRepository.findAll();
+    }
+    public Person findOne(int id){
+        return peopleRepository.findById(id).orElse(null);
+    }
+}
