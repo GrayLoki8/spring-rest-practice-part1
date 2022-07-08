e2608ec800edca9e9d9db36fd1c9913d80425045
Gray Loki <skuc090@gmail.com>
2022-07-08 14:16:04 +0300

    add PeopleController

--- src/main/java/ua/grayloki8/spring/springrestpractice/controllers/PeopleController.java
+++ src/main/java/ua/grayloki8/spring/springrestpractice/controllers/PeopleController.java
@@ -1,4 +1,30 @@
 package ua.grayloki8.spring.springrestpractice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ua.grayloki8.spring.springrestpractice.models.Person;
+import ua.grayloki8.spring.springrestpractice.sevices.PeopleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/people")
 public class PeopleController {
+
+    private PeopleService peopleService;
+    @Autowired
+    public PeopleController(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+    @GetMapping()
+    public List<Person> getPeople(){
+        return peopleService.findAll();
+    }
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable("id")int id){
+        return     peopleService.findOne(id);
+    }
 }
